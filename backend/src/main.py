2a4df0c5f3b20f0042bb607a5cde6f0afb5616f0
Gaizka Menéndez Hernández <gaizkam20@gmail.com>
2025-08-23 18:47:44 +0200

    Secrest and env variables

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+MY_PROJECT = os.environ.get("MY_PROJECT")
+API_KEY = os.environ.get("API_KEY")
+if not API_KEY or not MY_PROJECT:
+    raise NotImplementedError("API_KEY or MY_PROJECT were not set properly")
+
 @app.get("/")
 def read_index():
-    return {"hello": "world again"}
+    return {"hello": "world again", "Actual project": MY_PROJECT, "Api key": "elou"}
