75d28c8797e98d9bf9c46449f3eaeb85d4524461
Gaizka Menéndez Hernández <gaizkam20@gmail.com>
2025-09-05 22:54:19 +0200

    Postgresql connection added

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,26 @@
 import os
 from fastapi import FastAPI
+from api.db import init_db
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # before app starts
+    init_db() # we make sure the db is already up before starting the app
+    yield
+    # after app startup
 
-MY_PROJECT = os.environ.get("MY_PROJECT")
-API_KEY = os.environ.get("API_KEY")
-if not API_KEY or not MY_PROJECT:
-    raise NotImplementedError("API_KEY or MY_PROJECT were not set properly")
+app = FastAPI(lifespan=lifespan)
+
+# MY_PROJECT = os.environ.get("MY_PROJECT") or "This is my project"
+# API_KEY = os.environ.get("API_KEY")
+# if not API_KEY or not MY_PROJECT:
+#     raise NotImplementedError("API_KEY or MY_PROJECT were not set properly")
 
 @app.get("/")
 def read_index():
-    return {"hello": "world again", "Actual project": MY_PROJECT}
+    return {"hello": "world again"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "Everything ok"}
